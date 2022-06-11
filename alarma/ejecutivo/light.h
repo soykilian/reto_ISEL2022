008848e38812ad387ac9fec733bf3732e1179daf
soykilian <mariavdacosta.27@gmail.com>
2022-06-11 15:49:19 +0200

    Cleaning trash

--- alarma/ejecutivo/light.h
+++ alarma/ejecutivo/light.h
@@ -1,9 +1,11 @@
 #ifndef LIGHT_H
 # define LIGHT_H
 #include "alarma.h"
-
-
+#include "fsm.h"
 int light;
 int presencia;
-fsm_t* fsm_new_light(void);
+
+void timeval_sub (struct timeval *res, struct timeval *a, struct timeval *b);
+void timeval_add (struct timeval *res, struct timeval *a, struct timeval *b);
+int timeval_less (struct timeval *a, struct timeval *b);
 #endif
